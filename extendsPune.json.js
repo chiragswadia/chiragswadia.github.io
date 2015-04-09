dac19b642dc829bfc0f11d00ae9eb323b5644f87
Chirag Swadia <chirag.swadia@gmail.com>
2015-04-10 00:07:31 +0530

    test js file

---
+++ extendsPune.json.js
@@ -0,0 +1,57 @@
+[
+  {
+    "id": 1,
+    "company_name": "PubMatic",
+    "location": "Baner",
+    "technologies": "Multiple Technologies",
+    "careers_page": "http://pubmatic.com/careers.php",
+    "logo_url": "http://mobilemarketingmagazine.com/wp-content/uploads/2014/02/PubMatic-logo-460x261.jpg"
+  },
+
+  {
+    "id": 2,
+    "company_name": "Symantec India",
+    "location": "Baner",
+    "technologies": "Multiple Technologies",
+    "careers_page": "http://www.symantec.com/en/in/about/careers/current_openings.jsp",
+    "logo_url": "http://www.decorous-uk.com/wp-content/uploads/symantec-logo.jpg"
+  },
+  
+  {
+    "id": 3,
+    "company_name": "ShopSocially",
+    "location": "Pashan",
+    "technologies": "Social Media, Analytics, Python, UI, UX, Javascript",
+    "careers_page": "http://shopsocially.com/platform/careers",
+    "logo_url": "http://www.starkmedia.com/data/content_images/shopsocially.jpg"
+  },
+  
+  {
+    "id": 4,
+    "company_name": "TastyKhana",
+    "location": "Kalyani Nagar",
+    "technologies": "Multiple Technologies",
+    "careers_page": "http://tastykhana.in/careers",
+    "logo_url": "http://techlomedia.in/wp-content/uploads/2014/11/TK_new.png"
+  },
+  
+  {
+    "id": 5,
+    "company_name": "Druva",
+    "location": "SB Road, Shivaji Nagar",
+    "technologies": "Multiple Technologies",
+    "careers_page": "http://www.druva.com/about/careers/",
+    "logo_url": "http://www.pantropic.com.sg/Portals/0/druva-logo.jpg"
+  },
+  
+  {
+    "id": 6,
+    "company_name": "rtCamp",
+    "location": "Koregaon Park",
+    "technologies": "WordPress, PHP, Python, Ruby on Rails",
+    "careers_page": "https://rtcamp.com/careers/",
+    "logo_url": "http://mumbai.wordcamp.org/2015/files/2015/02/rtcamp-logo-whitebg-400.jpg"
+  }
+
+  
+  ]
